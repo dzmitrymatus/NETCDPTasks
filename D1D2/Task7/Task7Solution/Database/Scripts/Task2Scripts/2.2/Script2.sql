ec77810d1cc7f76b2441d89b0d814d532edfbf81
Dzmitry Matus <Dzmitry_Matus@epam.com>
2019-08-23 13:20:16 +0300

    [Task 7] add task 7.2 scripts

---
+++ D1D2/Task7/Task7Solution/Database/Scripts/Task2Scripts/2.2/Script2.sql
@@ -0,0 +1,6 @@
+﻿SELECT ord.EmployeeID AS SellerId, 
+		(SELECT CONCAT(em.FirstName,' ', em.LastName) FROM Employees AS em WHERE em.EmployeeID = ord.EmployeeID ) AS SellerName,
+		COUNT(*) AS Amount
+FROM Orders as ord
+GROUP BY ord.EmployeeID
+ORDER BY COUNT(*) desc
---
+++ D1D2/Task7/Task7Solution/Database/Scripts/Task2Scripts/2.2/Script3.sql
@@ -0,0 +1,5 @@
+﻿SELECT ord.EmployeeID, ord.CustomerID, COUNT(*) AS Amount, YEAR(ord.OrderDate) AS [Year]
+FROM Orders AS ord
+WHERE YEAR(ord.OrderDate) = 1998
+GROUP BY ord.EmployeeID, ord.CustomerID, YEAR(ord.OrderDate)
+ORDER BY COUNT(*) desc
